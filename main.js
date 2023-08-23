cd48bfe7720bac6aca9997eebb9dde36290e5392
khalidAlansari37 <123829439+khalidAlansari37@users.noreply.github.com>
2023-08-23 15:05:24 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+const summary = document.querySelector(".changer");
+function promptUser() {
+    var password = prompt("Please enter your password:");
+    if(password == "secret"){
+        summary.textContent = prompt("modify it")
+    }else{
+        alert("Incorrect password.");
+    }
+}
+function promptUser2() {
+    var password = prompt("Please enter your password:");
+    if(password == "secret"){
+        summary.textContent = prompt("modify it")
+    }else{
+        alert("Incorrect password.");
+    }
+}
+function promptUser3() {
+    var password = prompt("Please enter your password:");
+    if(password == "secret"){
+        summary.textContent = prompt("modify it")
+    }else{
+        alert("Incorrect password.");
+    }
+}
+function promptUser4() {
+    var password = prompt("Please enter your password:");
+    if(password == "secret"){
+        summary.textContent = prompt("modify it")
+    }else{
+        alert("Incorrect password.");
+    }
+}
+function promptUser5() {
+    var password = prompt("Please enter your password:");
+    if(password == "secret"){
+        summary.textContent = prompt("modify it")
+    }else{
+        alert("Incorrect password.");
+    }
+}
